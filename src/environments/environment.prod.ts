af797db775caf323e1258f480692225b1eddb01b
manojavula79 <manojavula79@gmail.com>
2026-05-29 19:24:15 +0530

    fix: real domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://way2wear-backend-production.up.railway.app/api/v1',
+  apiUrl: 'https://api.way2wear.in', //'https://way2wear-backend-production.up.railway.app/api/v1',
   openaiModel: 'gpt-4o',
   appVersion: '1.0.0',
   amazonAffiliateTag: 'way2wear-20',
